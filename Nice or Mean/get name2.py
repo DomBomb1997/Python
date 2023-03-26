c4fcb4db13d7c9e7aea9a29a8db67afe1da5592b
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2023-03-26 12:57:25 -0700

    submitted for grading

---
+++ Nice or Mean/get name2.py
@@ -0,0 +1,10 @@
+def start():
+    f_name = "Lucas"
+    l_name = "Vallios"
+    age = 28
+    gender = "Male"
+    get_info(f_name,l_name,age,gender)
+
+
+def get_info(f_name,l_name,age,gender):
+    print("My name is {0} {1}. I am {2} years old {3}.".format(f_name,l_name,age,gender))
