bb76c5e3df2c58204742eecda18390716d76cd1f
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2023-04-10 19:57:05 -0700

    Submitted for help

--- Projects/Testsql.py
+++ Projects/Testsql.py
@@ -5,10 +5,39 @@ conn = sqlite3.connect('test.db')
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_persons( \
-        ID INTEGER PRIMARY KEY AUTOINCREMENT. \
+        ID INTEGER PRIMARY KEY AUTOINCREMENT, \
         col_fname TEXT, \
         col_lname TEXT, \
         col_email TEXT \
         )")
     conn.commit()
 conn.close()
+
+
+
+conn = sqlite3.connect('test.db')
+
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
+                ("Sarah", "James", "Sarahjames@gmail.com"))
+    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
+                ("Sally", "May", "Sallymay@gmail.com"))
+    cur.execute("INSERT INTO tbl_persons(col_fname, col_lname, col_email) VALUES (?,?,?)", \
+                ("Robert", "Jones", "Robertjones@gmail.com"))
+    conn.commit()
+conn.close()
+
+
+conn = sqlite3.connect('test.db')
+
+
+with conn:
+    cur = conn.cursor()
+    cur.execute("SELECT col_fname, col_lname, col_email FROM tbl_persons WHERE col_fname = 'Sarah'")
+    varPerson = cur.fetchall()
+    for item in varPerson:
+        msg = "First Name: ()\nLastName: ()\nEmail: ()".format(item{0}.item{1}.item{2})
+print(msg)
+
