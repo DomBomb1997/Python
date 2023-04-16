c019b03abbf182191a745dd47fc2923df976028a
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2023-04-15 21:40:37 -0700

    Submitted for grading

---
+++ Inheritance Submission/Class.py
@@ -0,0 +1,14 @@
+class User:
+    name = 'Steam'
+    email = ' '
+    password = 'Steam123'
+    steam_account_ = 0
+
+class Employee(User)
+    base_pay = 15.00
+    department = 'Web Design'
+
+class Customer(User):
+    play_time = ''
+    gamess_owned = ''
+    
