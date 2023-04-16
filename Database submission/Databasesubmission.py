fb47848e4807bbd1d04b9f7b14fb4eeeb3c40d6b
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2023-04-15 20:55:03 -0700

    Resubmitted for grading

--- Database submission/Databasesubmission.py
+++ Database submission/Databasesubmission.py
@@ -10,7 +10,7 @@ with conn:
     conn.commit()
 conn.close()
 #creates the database if there isn't one
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('Files.db')
 #connects to the database again
 
 Filelist = ('Information.docx', 'Hello.txt', 'myImage.png', 'myMovie.mpg', 'World.txt','data.pdf','myPhoto.jpg')
@@ -21,7 +21,7 @@ for x in Filelist:
             cur = conn.cursor()
             # The value for each row will be one name out of the tuple therefore (x,)
             # will denote a one element tuple for each name ending in txt
-            cur.execute('INSERT INTO tbl_Files (cole_Files) VALUES (?)', (x,))
+            cur.execute('INSERT INTO tbl_Files (col_Files) VALUES (?)', (x,))
             print(x)
 
 conn.close
