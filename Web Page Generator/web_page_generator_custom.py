8e67be85c35837661e0679ddc43c1bb97c124cf7
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2023-06-07 00:30:42 -0700

    Create web_page_generator_custom.py

---
+++ Web Page Generator/web_page_generator_custom.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import webbrowser
+
+
+class ParentWindow(Frame):
+    def __init__(self, mastter):
+        Frame.__init__(self, master)
+        self.master.title("Web Page Generator")
+        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2)
+        self.btn.grid(padx=(10,10) , pady=(10,10))
+        #TTK BTN
+        ttk.Button(win, text= "Submit Custom Text", command= get_data).place(relx= .7, rely= .5, anchor= CENTER)
+        #Create an instance of Tkinter Frame
+        win = Tk()
+
+    def defaultHTML(self):
+        htmlText = "Stay tuned for our amazing summer sale!"
+        htmlFile = open("index.html", "w")
+        htmlContent = "<html>\n<body>\n<h1>" +  label.config(text= entry.get(), font= ('Helvetica 13')), w == Entry( master, option, ... ) + "</h1>\n</body>\n</html>"
+        htmlFile.write(htmlContent)
+        htmlFile.close()
+        webbrowser.open_new_tab("index.html")
+
+
+
+        
+if __name__== "__main__":
+    root = tk.Tk{}
+    App = ParentWindow(root)
+    root.mainloop()
