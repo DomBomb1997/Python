cb0eda249172048411474c2d5cce8e9bc7c58efb
DomBomb1997 <53356399+DomBomb1997@users.noreply.github.com>
2023-08-02 18:52:52 -0700

    Submitted for help

--- Web Page Generator/web_page_generator.py
+++ Web Page Generator/web_page_generator.py
@@ -4,11 +4,29 @@ import webbrowser
 
 
 class ParentWindow(Frame):
-    def __init__(self, mastter):
+    def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
-        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2)
+        self.btn = Button(self.master, text="Default HTML Page", width=30, height=1, command=self.defaultHTML)
         self.btn.grid(padx=(10,10) , pady=(10,10))
+        self.btn.grid(row=2, column=2, padx=(20, 10), pady=(30,0))
+
+        #Creates custom body button
+        self.Custombtn = Button(text="Custom Body", width=30, height=1, )
+        #Postions button on grid()
+        self.Custombtn.grid(row=2, column=1, padx=(20, 10), pady=(30,0))
+
+        
+        #Label
+        #tk.geometry("450x300") 
+   
+        # the label
+        Custombody = "Custom Body"
+        self.Label = tk.Label(text = Custombody)
+        self.Label.grid(row=0, column=0)
+        #Entry for custom body
+        self.customentry = Entry(width=75) #ommand=self.custom.body)
+        self.customentry.grid(row=1, column=0, padx=(20, 10), pady=(15, 10))
 
     def defaultHTML(self):
         htmlText = "Stay tuned for our amazing summer sale!"
@@ -20,6 +38,6 @@ class ParentWindow(Frame):
 
         
 if __name__== "__main__":
-    root = tk.Tk{}
+    root = tk.Tk()
     App = ParentWindow(root)
     root.mainloop()
--- Web Page Generator/web_page_generator_custom.py
+++ Web Page Generator/web_page_generator_custom.py
@@ -5,12 +5,14 @@ import webbrowser
 
 
 class ParentWindow(Frame):
-    def __init__(self, mastter):
+    def __init__(self, master):
         Frame.__init__(self, master)
         self.master.title("Web Page Generator")
-        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2)
+        self.btn = Button(self.master, text="Default HTML Page", width=30, height=2, command=self.defaultHTML)
         self.btn.grid(padx=(10,10) , pady=(10,10))
         #TTK BTN
+        self.custombtn = Button(self.master, text="Submit Custom Text", width=30, height=2, command=self.defaultHTML)
+        self.btn.grid(padx=(10,10) , pady=(10,10))        
         ttk.Button(win, text= "Submit Custom Text", command= get_data).place(relx= .7, rely= .5, anchor= CENTER)
         #Create an instance of Tkinter Frame
         win = Tk()
@@ -20,7 +22,7 @@ class ParentWindow(Frame):
 
         # Define a function to return the Input data
     def get_data():
-       label.config(text= entry.get(), font= ('Helvetica 13'))
+        label.config(text= entry.get(), font= ('Helvetica 13'))
 
         #Inititalize a Label widget
         label= Label(win, text="", font=('Helvetica 13'))
@@ -30,7 +32,7 @@ class ParentWindow(Frame):
     def defaultHTML(self):
         htmlText = "Stay tuned for our amazing summer sale!"
         htmlFile = open("index.html", "w")
-        htmlContent = "<html>\n<body>\n<h1>" + command= get_data + "</h1>\n</body>\n</html>"
+        htmlContent = "<html>\n<body>\n<h1>" + htmlText + "</h1>\n</body>\n</html>"
         htmlFile.write(htmlContent)
         htmlFile.close()
         webbrowser.open_new_tab("index.html")
@@ -39,6 +41,6 @@ class ParentWindow(Frame):
 
         
 if __name__== "__main__":
-    root = tk.Tk{}
+    root = tk.Tk()
     App = ParentWindow(root)
     root.mainloop()
